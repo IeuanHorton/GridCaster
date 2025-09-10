88ad150969a311a1f90cb47f729ad1a0f01e0ef3
IeuanHorton <HortonIeuan@gmail.com>
2025-09-10 02:20:53 +0000

    fixed spelling and added a yellow pixel

--- raycaster.cpp
+++ raycaster.cpp
@@ -5,9 +5,21 @@
 int const WINDOWWIDTH = 1024;
 int const WINDOWHEIGHT = 512;
 
+float pX,pY; //Player Position
+	     
+void drawPlayer()
+{
+	glColor3f(1,1,0); //Yellow
+	glPointSize(8);
+	glBegin(GL_POINTS);
+	glVertex2i(pX,pY);
+	glEnd();
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	drawPlayer();
 	glutSwapBuffers();
 }
 
@@ -15,13 +27,15 @@ void init()
 {
 	glClearColor(0.3,0.3,0.3,0);
 	gluOrtho2D(0,WINDOWWIDTH,WINDOWHEIGHT,0);
+
+	pX=300; pY=300;
 }
 
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(WINDOWWIDTH,WINDOWGEIGHT);
+	glutInitWindowSize(WINDOWWIDTH,WINDOWHEIGHT);
 	glutCreateWindow("RAYCASTERTEST");
 	init();
 	glutDisplayFunc(display);
